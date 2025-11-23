5ab7432ecac896adb44716e363bffc067ed3714b
Jineth <jinethd@gmail.com>
2025-11-23 16:26:09 +0000

    concurrency task

---
+++ basicConcurrency/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// we seek to sum the numbers 1-1000000 using 4 separate threads for each quarter of 1m
+
+int main(){
+
+    std::cout<<"Hello World"<<std::endl;
+
+    return 0;
+}
